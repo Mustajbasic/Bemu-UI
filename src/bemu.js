bbc62379ce087bb57cf63b218f53d98183774439
Mustajbasic <belmin.mustajbasic@gmail.com>
2017-10-16 16:26:45 +0200

    Separeted bemu.js into two files

--- src/bemu.js
+++ src/bemu.js
@@ -14,29 +14,6 @@ function Bemu() {
             }
         }
     };
-    public.http ={
-        get: function (url, callback) {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState === 4 && this.status === 200) {
-                    callback(this.responseText);
-                }
-            };
-            xhttp.open("GET", url, true);
-            xhttp.send();
-        },
-        post: function (url, data, callback) {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState === 4 && this.status === 200) {
-                    callback(this.responseText);
-                }
-            };
-            xhttp.open("POST", url, true);
-            xhttp.setRequestHeader("Content-type", "application/json");
-            xhttp.send(JSON.stringify(data));
-        }
-    }
     public.addSideNavigation = function (id, courtain) {
         sidenav = BemuSideNavigation(id, courtain);
 
